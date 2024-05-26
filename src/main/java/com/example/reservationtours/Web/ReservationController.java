8db77c500ea30639bcfee6f6fec3d48aa8570135
mohammed <Mohammed.Sanbouli@emsi-edu.ma>
2024-05-27 00:42:43 +0100

    ajout du controlleur de reservation

--- src/main/java/com/example/reservationtours/Web/ReservationController.java
+++ src/main/java/com/example/reservationtours/Web/ReservationController.java
@@ -1,7 +1,13 @@
 package com.example.reservationtours.Web;
 
+import com.example.reservationtours.DAO.Entities.Reservation;
 import com.example.reservationtours.Services.ReservationService;
+import com.example.reservationtours.Services.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,12 +15,29 @@ import java.util.List;
 @RestController
 public class ReservationController {
 
-    ReservationService service;
+    ReservationService reservationService;
+    TourService tourService;
 
     @Autowired
-    public ReservationController(ReservationService service){
-        this.service = service;
+    public ReservationController(ReservationService reservationService, TourService tourService) {
+        this.reservationService = reservationService;
+        this.tourService = tourService;
+    }
+    @GetMapping("/reservations/new/{tourId}")
+    public String showReservationForm(@PathVariable Long tourId, Model model) throws Exception {
+        Reservation reservation = new Reservation();
+        reservation.setTour(tourService.getTourById(tourId));
+        model.addAttribute("reservation", reservation);
+        model.addAttribute("tour",reservation.getTour());
+        model.addAttribute("user",reservation.getUser());
+        return "redirect:/reservation_form.html";
     }
 
-    //demain nkhdmoha hh
+    @PostMapping("/reservations")
+    public String makeReservation(Reservation reservation) {
+        reservationService.addReservation(reservation);
+        return "redirect:/tours/all";
+    }
+
+
 }
